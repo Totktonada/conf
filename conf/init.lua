7d7845f3f1d579b2290478b16282ad097dd99f25
Alexander Turenko <alexander.turenko@tarantool.org>
2021-03-25 05:55:32 +0300

    doc: initial description of the common API

--- conf/init.lua
+++ conf/init.lua
@@ -132,9 +132,38 @@ local supported_drivers = {
 --
 -- @section Functions
 
---- Cleate new instance.
+--- Create new configuration storage client instance.
 --
--- XXX: Write docs.
+-- @array[string] endpoints
+--     Endpoint URLs.
+-- @table         opts
+--     Client options.
+--
+--     May contain driver specific options (listed in
+--     @{conf.driver.etcd.new}).
+-- @string        opts.driver
+--     Driver name. Only 'etcd' is supported now.
+--
+--     It is the mandatory option.
+--
+-- @raise See 'General API notes' in the @{conf.driver.etcd|etcd
+-- driver documentation}.
+--
+-- XXX: Move the General API notes (at least errors) outside of
+-- the etcd driver documentation.
+--
+-- @return client instance.
+--
+-- @usage
+--
+-- local conf_lib = require('conf')
+--
+-- local urls = {
+--     'http://localhost:2379',
+--     'http://localhost:2381',
+--     'http://localhost:2383',
+-- }
+-- local conf = conf.new(urls, {driver = 'etcd'})
 --
 -- @function conf.new
 local function new(endpoints, opts)
@@ -162,7 +191,60 @@ end
 
 --- Fetch a value.
 --
--- XXX: Write docs.
+-- Note: A string value that can be interpreted as a number will
+-- be converted to a number. It is constraint of current
+-- implementation and will be resolved in a future.
+--
+-- @param  self
+--     Client instance.
+-- @string key
+--     A key to fetch.
+--
+--     A dot notation may be used to access a nested map field or
+--     array element: `'foo.bar'` or `'foo.1'`. Array indices are
+--     1-based, just as in Lua.
+--
+--     XXX: Describe the dot notition is the separate section
+--     (like 'General API notes').
+--
+-- @raise See 'General API notes' in the @{conf.driver.etcd|etcd
+-- driver documentation}.
+--
+-- XXX: Move the General API notes (at least errors) outside of
+-- the etcd driver documentation.
+--
+-- @return Response of the following structure:
+--
+-- ```
+-- {
+--     data = obj,
+-- }
+-- ```
+--
+-- `obj` is the value associated with given key or `nil` if given
+-- key does not exist.
+--
+-- @usage
+--
+-- -- Put a map.
+-- conf:set('foo', {bar = {baz = 42}})
+--
+-- -- Fetch it back.
+-- local res = conf:get('foo')
+-- -- res.data is {bar = {baz = 42}}
+--
+-- -- Or fetch a field.
+-- local res = conf:get('foo.bar')
+-- -- res.data is {baz = 42}
+-- local res = conf:get('foo.bar.baz')
+-- -- res.data is 42
+--
+-- -- Put an array.
+-- conf:set('foo', {'a', 'b', 'c'})
+--
+-- -- Fetch an array element.
+-- local res = conf:get('foo.1')
+-- -- res.data is 'a'
 --
 -- @function instance.get
 local function get(self, key)
@@ -182,7 +264,52 @@ end
 
 --- Store a value.
 --
--- XXX: Write docs.
+-- If given key contains a value, the value is **replaced** with
+-- the new one. Maps are NOT merged.
+--
+-- Use dot notation to set a particular field of a map (or
+-- particular element of an array).
+--
+-- Storing of `foo.bar.baz` means that `foo` and `foo.bar` will
+-- store a map. The same for `foo.1`: `foo` will store an array.
+--
+-- XXX: Define behaviour for corner cases like storing `foo.bar`,
+-- when `foo` exists and it is not a map.
+--
+-- @param  self
+--     Client instance.
+-- @string key
+--     A key to set.
+--
+--     A dot notation may be used, see @{conf.get}.
+-- @param  obj
+--     A value to store.
+--
+--     Allowed types: `string`, `number`, `table` (a map or an
+--     array).
+--
+-- @raise See 'General API notes' in the @{conf.driver.etcd|etcd
+-- driver documentation}.
+--
+-- XXX: Move the General API notes (at least errors) outside of
+-- the etcd driver documentation.
+--
+-- @return Nothing.
+--
+-- It is recommended to don't lean on the fact that the method
+-- does not return anything, because the API can be extended with
+-- a return value in a future.
+--
+-- @usage
+--
+-- -- Store a scalar, a map and an array.
+-- conf:set('foo', 'hello')
+-- conf:set('moo', {bar = {baz = 42}})
+-- conf:set('xoo', {'a', 'b', 'c'})
+--
+-- -- Set a nested field and an array element.
+-- conf:set('foo.bar.baz', 'hello')
+-- conf:set('foo.1', 'hello')
 --
 -- @function instance.set
 local function set(self, key, obj)
@@ -196,7 +323,41 @@ end
 
 --- Delete a value.
 --
--- XXX: Write docs.
+-- XXX: Define behaviour for tricky cases like removing an array
+-- element in a middle of the array.
+--
+-- @param  self
+--     Client instance.
+-- @string key
+--     A key to delete.
+--
+--     A dot notation may be used, see @{conf.get}.
+--
+-- @raise See 'General API notes' in the @{conf.driver.etcd|etcd
+-- driver documentation}.
+--
+-- XXX: Move the General API notes (at least errors) outside of
+-- the etcd driver documentation.
+--
+-- @return Nothing.
+--
+-- It is recommended to don't lean on the fact that the method
+-- does not return anything, because the API can be extended with
+-- a return value in a future.
+--
+-- @usage
+--
+-- -- Store a map and delete it then.
+-- conf:set('foo', {bar = {baz = 42}})
+-- conf:del('foo')
+--
+-- -- Or delete just one field.
+-- conf:set('foo', {bar = {baz = 42}})
+-- conf:del('foo.bar.baz')
+--
+-- -- Or delete an array element.
+-- conf:set('foo', {'a', 'b', 'c'})
+-- conf:del('foo.3')
 --
 -- @function instance.del
 local function del(self, key)
